d5d119bab96423331689664e08a838f3c1427373
Sviatlana Kazachok <34425237+skazachok@users.noreply.github.com>
2018-02-20 23:42:57 -0500

    I counted number of Love triangles
    I counted number of Love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count=0;
+  for(var i = 0 ;i < preferences.length; i++) {
+    const lover1=i+1;
+    const lover1target=preferences[i];
+    const lover2target=preferences[lover1target-1];
+    const lover3target=preferences[lover2target-1];
+    if (lover1===lover3target) {
+      count++;
+    }
+  }
+  return Math.floor(count/3);
 };
